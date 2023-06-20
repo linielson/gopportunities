6d577eb9086ded5f89796672cbef428da3ce63d0
Linielson Rosa <linielson.rosa@generalassemb.ly>
2023-06-20 11:59:29 +0200

    feat: add routes

--- router/router.go
+++ router/router.go
@@ -4,10 +4,9 @@ import "github.com/gin-gonic/gin"
 
 func Initialize() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	router.Run(":8080")
+	initializeRoutes(router)
+	err := router.Run(":8080")
+	if err != nil {
+		return
+	}
 }
---
+++ router/routes.go
@@ -0,0 +1,41 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func initializeRoutes(router *gin.Engine) {
+	v1 := router.Group("/api/v1/")
+	{
+		v1.GET("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "Get Opening",
+			})
+		})
+
+		v1.POST("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "Post Opening",
+			})
+		})
+
+		v1.PUT("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "Put Opening",
+			})
+		})
+
+		v1.DELETE("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "Delete Opening",
+			})
+		})
+
+		v1.GET("/openings", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "Get Openings",
+			})
+		})
+	}
+}
